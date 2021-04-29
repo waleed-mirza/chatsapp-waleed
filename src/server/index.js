6cfbba51adaa9205498cdda35dfeaf1fa8b4076d
unknown <waleedmirza667@gmail.com>
2021-04-29 08:31:09 -0700

    changes make for doplyement

--- src/server/index.js
+++ src/server/index.js
@@ -7,8 +7,7 @@ const PORT = process.env.PORT || 3001;
 // app.get("/", (req, res) => {
 //   res.send("<h1>Hello world</h1>");
 // });
-app.use;
-express.static(__dirname + "/../../build");
+app.use(express.static(__dirname + "/../../build"));
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("chat message", (msg) => {
